2a3162fcaaf1f374edfea457d30d97d204c604ea
Nick Foster <nfost18@gmail.com>
2023-11-18 02:05:23 -0500

    Added better stone tiles and implemented health

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public Animator[] heartAnimators; // Array of heart Animators
+    public Sprite halfHeartSprite;
+    public void UpdateHealth(int currentHealth)
+    {
+        for (int i = 0; i < heartAnimators.Length; i++)
+        {
+            if (i < currentHealth / 2)
+                heartAnimators[i].Play("FullHeart");
+            else if (i * 2 < currentHealth)
+                heartAnimators[i].SetTrigger("ToHalf");
+            else
+                heartAnimators[i].SetTrigger("ToEmpty");
+        }
+    }
+    public void SetFirstHeartToHalfIdle()
+    {
+        int currentHeart = 0;
+        if (heartAnimators.Length > currentHeart)
+        {
+            heartAnimators[currentHeart].Play("HalfIdle"); // Play the HalfIdle animation for the first heart
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int maxHealth = 6;
+    private int currentHealth;
+
+    // Reference to the HealthUI script
+    public HealthUI healthUI;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        // Update the health UI at the start of the game
+        healthUI.UpdateHealth(currentHealth);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Call TakeDamage with a fixed damage amount, e.g., 1
+            TakeDamage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            Heal(1);
+        }
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        currentHealth = Mathf.Max(currentHealth, 0);
+        // Update the health UI
+        healthUI.UpdateHealth(currentHealth);
+    }
+
+    public void Heal(int healAmount)
+    {
+        bool wasAtZero = currentHealth == 0;
+        currentHealth += healAmount;
+        currentHealth = Mathf.Min(currentHealth, maxHealth);
+        // Update the health UI
+        if (wasAtZero && currentHealth > 0)
+        {
+            healthUI.SetFirstHeartToHalfIdle(); // Call this when healing from zero
+        }
+        else
+        {
+            healthUI.UpdateHealth(currentHealth);
+        }
+    }
+}
