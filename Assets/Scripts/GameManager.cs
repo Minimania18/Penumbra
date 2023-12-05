23b88238a029a77078b3bf142694df86a3bf30f4
Nick Foster <nfost18@gmail.com>
2023-12-05 06:03:11 -0500

    Added fading coroutine transition when changing scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public int PlayerHealth { get; set; }
     public GameObject NextSpawnPoint { get; private set; }
-
     public bool canAttack { get; private set; } = false;
+
+    public Image fadeImage;
+    public float fadeDuration = 1f;
 
     private void Awake()
     {
@@ -21,7 +26,7 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        PlayerHealth = 100;
+       //PlayerHealth = 100;
     }
 
     public void TakeDamage(int damageAmount)
@@ -45,14 +50,40 @@ public class GameManager : MonoBehaviour
     }
     public void TransitionToScene(string sceneName, string spawnPointName)
     {
+        StartCoroutine(FadeAndLoadScene(sceneName, spawnPointName));
+    }
+
+    private IEnumerator FadeAndLoadScene(string sceneName, string spawnPointName)
+    {
+        // Fade out
+        yield return StartCoroutine(Fade(1));
+
+        // Set spawn point and load new scene
         PlayerPrefs.SetString("NextSpawnPoint", spawnPointName);
         Debug.Log("Transitioning to scene: " + sceneName + " with spawn point: " + spawnPointName);
         SceneManager.LoadScene(sceneName);
+
+        // Fade in
+        yield return StartCoroutine(Fade(0));
     }
-   /* public void ResetSpawnPointToDefault()
+
+    private IEnumerator Fade(float targetAlpha)
     {
-        // Set the default spawn point name
-        PlayerPrefs.SetString("NextSpawnPoint", "DefaultSpawnPointName");
+        float startAlpha = fadeImage.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(0f, 0f, 0f, newAlpha);
+            yield return null;
+        }
     }
-   */
+    /* public void ResetSpawnPointToDefault()
+     {
+         // Set the default spawn point name
+         PlayerPrefs.SetString("NextSpawnPoint", "DefaultSpawnPointName");
+     }
+    */
 }
